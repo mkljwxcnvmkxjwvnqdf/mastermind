6011eb406c073a4ae4d5ff058897d66fadd1b6f0
Hazblow <lolondon19.chevalier14@gmail.com>
2021-10-29 15:40:15 +0200

    zzdz

--- src/mastermind/Main.java
+++ src/mastermind/Main.java
@@ -3,7 +3,7 @@ package mastermind;
 import mastermind.View.PlateauView;
 
 public class Main {
-
+    final Integer MAX_TENTATIVES = 10;
     public static void main(String[] args)
     {
         new PlateauView();
